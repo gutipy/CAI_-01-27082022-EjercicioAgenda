28fb6a7fe8e24fa8847ffac29c1c80eaa35f7bc1
Agustin Gutierrez <58868512+gutipy@users.noreply.github.com>
2022-08-27 18:18:45 -0300

    Ejercicio Agenda
    Primer set de cambios correspondientes al Ejercicio Agenda.

---
+++ ProyectoAgenda/ProyectoAgenda.InterfazConsola/Contacto.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAgenda.InterfazConsola
+{
+    public class Contacto
+    {
+        //Atributos de la clase Contacto
+        private string _nombre;
+        private string _apellido;
+        private string _telefono;
+        private string _direccion;
+        private DateTime _fechaNacimiento;
+        private int _llamadas;
+
+        //Constructor de la clase Contacto
+        public Contacto(string nombre, string apellido, string telefono, string direccion, DateTime fechaNacimiento)
+        {
+            _nombre = nombre;
+            _apellido = apellido;
+            _telefono = telefono;
+            _direccion = direccion;
+            _fechaNacimiento = fechaNacimiento;
+        }
+
+        //Propiedades de la clase Contacto
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return _apellido;
+            }
+        }
+
+        public string Telefono
+        {
+            get
+            {
+                return _telefono;
+            }
+        }
+
+        public string Direccion
+        {
+            get
+            {
+                return _direccion;
+            }
+        }
+
+        public DateTime FechaNacimiento
+        {
+            get
+            {
+                return _fechaNacimiento;
+            }
+        }
+
+        public int Llamadas
+        {
+            get
+            {
+                return _llamadas;
+            }
+        }
+
+
+        //Función que calcula la edad de un contacto
+        public int Edad()
+        {
+            int edad;
+
+            edad = (DateTime.Now - _fechaNacimiento).Days / 365;
+
+            return edad;
+        }
+    }
+}
---
+++ ProyectoAgenda/ProyectoAgenda.InterfazConsola/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAgenda.InterfazConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contacto C1 = new Contacto("agu", "guti", "111", "calle falsa 123", DateTime.Now.AddYears(-25));
+
+            Console.WriteLine(C1.ToString());
+
+            Console.WriteLine(C1.Edad());
+
+            Console.ReadKey();
+        }
+    }
+}
